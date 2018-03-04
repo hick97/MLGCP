12775573e5dd4a2ab4e01ef280c55493677c44ff
Henrique Augusto <hick_97@hotmail.com>
2018-03-04 13:19:53 -0300

    Add files via upload

---
+++ MGLcut/main.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Solucao.h"
+
+using namespace std;
+
+int main(void){
+	// Recebe a quantidade de vértices e cores.
+	int nVertex;
+	int nLabels;
+	scanf("%d %d", &nVertex, &nLabels);
+
+	Grafo g(nVertex, nLabels);
+
+	// Prennche Matriz.
+	g.FillsMatrix();
+
+	Solucao sol(&g);
+
+	// Divide os vertices em dois grupos.
+        for (int i = 0; i < g.n; i++) {
+
+            if (i < (g.n / 2)) {
+                sol.left.push_back(i);
+            } else {
+                sol.right.push_back(i);
+
+            }
+        }
+        printf("Parte esquerda: \n");
+        for (int i = 0; i < sol.left.size(); i++) {
+            printf("%d", sol.left[i]);
+
+        }
+        printf("\nParte direita: \n");
+        for (int i = 0; i < sol.right.size(); i++) {
+            printf("%d", sol.right[i]);
+
+        }
+        printf("\n");
+
+        //Frequencia de cada cor.
+        sol.f();
+
+        //Imprime a frequencia.
+        printf("Frequencia: ");
+        for (int i = 0; i < g.nLabels; i++) {
+            printf("%d", sol.cFreq[i]);
+        }
+        //Imprime o numero de cores:
+        printf("\nNumero de cores: %d \n", sol.aval);
+        //Teste: mudança na posição 0;
+        sol.leftoright(0);
+        printf("Pos- mundaca: \n");
+        //Imprime a frequencia.
+        printf("Frequencia: ");
+        for (int i = 0; i < g.nLabels; i++) {
+            printf("%d", sol.cFreq[i]);
+        }
+        //Imprime o numero de cores:
+        printf("\nNumero de cores: %d \n", sol.aval);
+        sol.rightoleft(3);
+        printf("Pos- mundaca: \n");
+        //Imprime a frequencia.
+        printf("Frequencia: ");
+        for (int i = 0; i < g.nLabels; i++) {
+            printf("%d", sol.cFreq[i]);
+        }
+        //Imprime o numero de cores:
+        printf("\nNumero de cores: %d \n", sol.aval);
+
+
+
+        //getchar();
+
+	return 0;
+}
