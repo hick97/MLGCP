d1fcbdd2dc4742909941b1a5cf6805255f78896d
Henrique Augusto <hick_97@hotmail.com>
2018-03-06 21:32:03 -0300

    Add files via upload

---
+++ MGLcut/Solucao.cpp
@@ -0,0 +1,126 @@
+#include "Solucao.h"
+#include<stdio.h>
+#include <iostream>
+
+//Recebe um ponteiro para um grafo e inicia cFreq, aval com 0's.
+Solucao::Solucao(Grafo *grafo) : cFreq(grafo->nLabels, 0), aval(0), g(grafo)
+{
+}
+// Encontra o meu aval.
+int Solucao::f()
+{
+    this->aval = 0;
+	//Acessa as listas da esquerda e direita e verifica a frequência de cada cor.
+	for (int i = 0; i < this->left.size(); i++) {
+		for (int j = 0; j < this->right.size(); j++) {
+			printf("Comparando: %d com %d\n ", this->left[i], this->right[j]);
+			int color = this->g->M[left[i]][right[j]];
+			if (color != this->g->nLabels) {
+				printf("Cor: %d\n", color);
+				// verifca se a cor já foi computada.
+				if (this->cFreq[color] == 0) {
+					this->aval++;
+					this->cFreq[color]++;
+				} else {
+					this->cFreq[color]++;
+				}
+			}else{
+				printf("Sem cor!\n");
+			}
+          }
+
+      }
+      return this->aval;
+}
+// Movimento esquerda-direta.
+void Solucao::leftoright (int pos){
+    printf("Movimento ESQUERDA-DIREITA\n");
+// Garantindo que meu iterador sempre ficará no inicio do vector.
+    this->it2 = this->left.begin();
+    // Referenciando posição a ser apagada.
+    it2 = it2 + pos;
+
+    // Adciona na direita e o apaga na esquerda
+    this->right.push_back(this->left[pos]);
+    this->left.erase(this->it2);
+
+    // verifica as ligações com o lado esquerdo.
+    for(int i = 0; i<this->left.size();i++){
+            // Recebe a "cor" do ultimo elemento da direita e compara com a esquerda.
+            int color = this->g->M[right[right.size()-1]][left[i]];
+        //Incrementação do aval e frequencia.
+            if (color != this->g->nLabels) {
+               // printf("Cor: %d\n", color);
+				// verifca se a cor já foi computada.
+				if (this->cFreq[color] == 0) {
+					this->aval++;
+					this->cFreq[color]++;
+				}else{
+                    this->cFreq[color]++;
+				}
+            }
+    }
+    //Verifica as ligações com o lado direito.
+    for(int i = 0; i<this->right.size();i++){
+
+           int color = this->g->M[right[right.size()-1]][right[i]];
+
+        //Decrementação do aval e frequencia.
+        if (color != this->g->nLabels) {
+            this->cFreq[color]--;
+            // verifica se a frequencia da cor foi pra zero, caso positivo, decremento aval.
+            if(cFreq[color]==0){
+                this->aval--;
+            }
+        }
+
+    }
+    return;
+
+}
+//Movimento direita-esquerda.
+void Solucao::rightoleft (int pos){
+    printf("Movimento DIREITA-ESQUERDA: \n");
+// Garantindo que meu iterador sempre ficará no inicio do vector.
+   this->it = this->right.begin();
+// Referenciando a posição a ser apagada.
+    it = it + pos;
+
+    this->left.push_back(this->right[pos]);
+    this->right.erase(this->it);
+
+    // verifica as ligações com o lado direito.
+    for(int i = 0; i<this->right.size();i++){
+
+            int color = this->g->M[left[left.size()-1]][right[i]];
+
+    //Incrementação do aval e frequencia.
+            if (color != this->g->nLabels) {
+                //printf("Cor: %d\n", color);
+				// verifca se a cor já foi computada.
+				if (this->cFreq[color] == 0) {
+					this->aval++;
+					this->cFreq[color]++;
+				}else{
+                    this->cFreq[color]++;
+				}
+            }
+    }
+    //Verifica as ligações com o lado esquerdo.
+    for(int i = 0; i<this->left.size();i++){
+
+        int color = this->g->M[left[left.size()-1]][left[i]];
+
+//Decrementação do aval e frequencia.
+        if (color != this->g->nLabels) {
+            this->cFreq[color]--;
+            // verifica se a frequencia da cor foi pra zero, caso positivo, decremento aval.
+            if(cFreq[color]==0){
+                this->aval--;
+            }
+        }
+
+    }
+    return;
+
+}
---
+++ MGLcut/main.cpp
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Solucao.h"
+#include <fstream>
+
+using namespace std;
+
+int main(void){
+    // Leitura do numero de vertices e labels.
+    std::ifstream file("matriz.txt");
+    if(!file){/*falhou em abrir o arquivo*/}
+
+    int nVertex;
+    if(!(file >> nVertex)){/*falhou em ler o primeiro valor*/}
+
+    int nLabels;
+    if(!(file >> nLabels)){/*falhou em ler o segundo valor*/}
+
+    //Preenche meu Grafo de acordo com o arquivo.
+	Grafo g(nVertex, nLabels);
+	for (int i = 0; i < g.n; i++) {
+            for (int j = i; j < g.n; j++) {
+                if(!(file >> g.M[i][j])) {/*falhou em ler um dos valores*/}
+
+                g.M[j][i] = g.M[i][j];
+                //Lista de adjacencia:
+                if(g.M[i][j] < g.nLabels)
+                {
+                    g.L[i].push_back(j);
+                    g.L[j].push_back(i);
+                }
+            }
+     }
+
+	Solucao sol(&g);
+
+	// Divide os vertices em dois grupos.
+        for (int i = 0; i < g.n; i++) {
+
+            if (i < (g.n / 2)) {
+                sol.left.push_back(i);
+            } else {
+                sol.right.push_back(i);
+
+            }
+        }
+        printf("Parte esquerda: \n");
+        for (int i = 0; i < sol.left.size(); i++) {
+            printf("%d", sol.left[i]);
+
+        }
+        printf("\nParte direita: \n");
+        for (int i = 0; i < sol.right.size(); i++) {
+            printf("%d", sol.right[i]);
+
+        }
+        printf("\n");
+
+        //Frequencia de cada cor.
+        sol.f();
+
+        //Imprime a frequencia.
+        printf("Frequencia: ");
+        for (int i = 0; i < g.nLabels; i++) {
+            printf("%d,", sol.cFreq[i]);
+        }
+        printf("\n");
+        //Imprime o numero de cores:
+        printf("Aval: %d \n", sol.aval);
+        //Teste: mudança na posição 0;
+        sol.leftoright(0);
+        printf("Pos- mundaca: \n");
+        //Imprime a frequencia.
+        printf("Frequencia: ");
+        for (int i = 0; i < g.nLabels; i++) {
+            printf("%d,", sol.cFreq[i]);
+        }
+        //Imprime o numero de cores:
+        printf("\nAval: %d \n", sol.aval);
+        sol.rightoleft(2);
+        printf("Pos- mundaca: \n");
+        //Imprime a frequencia.
+        printf("Frequencia: ");
+        for (int i = 0; i < g.nLabels; i++) {
+            printf("%d,", sol.cFreq[i]);
+        }
+        //Imprime o numero de cores:
+        printf("\nAval: %d \n", sol.aval);
+
+
+
+        //getchar();
+
+	return 0;
+}
