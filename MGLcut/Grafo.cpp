ddda9e168018431f99f31639cb6f4cc8f140263a
Henrique Augusto <hick_97@hotmail.com>
2018-03-23 02:18:50 -0300

    Delete Grafo.cpp

--- MGLcut/Grafo.cpp
+++
@@ -1,9 +0,0 @@
-#include "Grafo.h"
-#include <stdio.h>
-
-Grafo::Grafo(int nVert, int nl) : M(nVert), L(nVert), n(nVert), nLabels(nl)
-{
-     for(int i=0; i<this->n; ++i)
-           M[i].resize(this->n);
-}
-
