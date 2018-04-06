ce5e91a62c8774258180a03b408ffbc2cfd99877
Henrique Augusto <hick_97@hotmail.com>
2018-04-06 12:48:55 -0300

    Update Grafo.cpp

--- MGLcutST/Grafo.cpp
+++ MGLcutST/Grafo.cpp
@@ -17,7 +17,7 @@ Grafo::Grafo(int nVert, int nl) : M(nVert), L(nVert), Ml(nl), n(nVert), nLabels(
       
 }
 
-void Grafo::DFSUtil(int v, bool visited[])
+void Grafo::DFSVisitColor(int v, int cor, bool visited[])
 {
 
     // Mark the current node as visited and
@@ -27,12 +27,10 @@ void Grafo::DFSUtil(int v, bool visited[])
     printf("\n");
    // vizinhos.push_back(v);
 
-
-
     // Recur for all the vertices adjacent
     // to this vertex
     std::vector<int>::iterator i;
-    for (i = L[v].begin(); i != L[v].end(); ++i)
+    for (i = Ll[cor][v].begin(); i != Ll[cor][v].end(); ++i)
         if (!visited[*i])
             DFSUtil(*i, visited);
 
@@ -43,7 +41,7 @@ void Grafo::DFSUtil(int v, bool visited[])
 
 // DFS traversal of the vertices reachable from v.
 // It uses recursive DFSUtil()
-void Grafo::DFS(int v, Data *d)
+void Grafo::DFScolor(int v, int cor, Data *d)
 {
     //vector <int> vizinhos;
     //std::vector<int>::iterator *k = vizinhos.begin();
@@ -55,7 +53,8 @@ void Grafo::DFS(int v, Data *d)
 
     // Call the recursive helper function
     // to print DFS traversal
-     DFSUtil(v, visited);
+     DFSvisitColor(v, cor, visited);
+     d->virtualVertex.clear();// Limpa meus vertices encontrados na DFS.
      for(int k = 0;k <this->n;k++){
         if(visited[k] == true){
             d->virtualVertex.push_back(k);
@@ -65,96 +64,84 @@ void Grafo::DFS(int v, Data *d)
 //Adicionando minhas arestas virtuais no grafo atual.
 void Grafo::AddVirtualEdges(Data *d, int color){
     for(int i= 0; i<d->virtualVertex.size(); i++){
-        for(int j= i; j<d->virtualVertex.size(); j++){
-            if((this->M[d->virtualVertex[i]][d->virtualVertex[j]] != color) && (d->virtualVertex[i] != d->virtualVertex[j])){
-                printf("Adcionei %d entre os vertices %d e %d\n", color, d->virtualVertex[i], d->virtualVertex[j]);
-                this->M[d->virtualVertex[i]][d->virtualVertex[j]] = color;
+        int &v = d->virtualVertex[i];
+        for(int j = i+1; j<d->virtualVertex.size(); j++){
+            int &u = d->virtualVertex[j]; 
+            if(this->Ml[cor][v][u] == 0){
+                printf("Adcionei %d na aresta (%d, %d)\n", color, v, u);
+                this->Ml[cor][v][u] = 1;
+                this->Ml[cor][u][v] = 1;
+                this->Ll[cor][v].push_back(u);
+                this->Ll[cor][u].push_back(v);
+                
+                this->M[v][u].push_back(cor);
+                this->M[u][v].push_back(cor);
+                if(this->M[v][u].size() == 1)
+                {
+                     this->L[v].push_back(u);
+                     this->L[u].push_back(v);
+                }
             }
         }
     }
 }
 
-void Solucao::VirtualEdges(){
-    Data *d =  new Data; //Guarda os vertices da minha DFS;
+void Grafo::VirtualEdges()
+{
+    // Grafo original.
+    printf("Grafo ORIGINAL:\n");
+    for (int i = 0; i < this->g->n; i++) {
+            for (int j = 0; j < this->this->n; j++) {
+                vector<int> &cores = this->M[i][j];
+                 printf("{");
+                 for(int c=0; c < cores.size(); ++c)
+                    printf("%d ", cores[c]);
+                 printf("} ");
+            }
+            printf("\n");
+    }
+     
+     
+     Data *d =  new Data; //Guarda os vertices da minha DFS;
     vector<int>initialPos; // Vertices iniciais para DFS;
 
-    int aux = 0; // Variavell auxiliar (label atual);
-    Grafo gVirtual(this->g->n, this->g->nLabels); // Meu grafo com arestas virtuais;
-
-   // while(aux != this->g->nLabels){ // Percorre todas as minhas labels, montando as matrizes de cada uma.
-
-        Grafo gAtual(this->g->n, this->g->nLabels); // Grafo de label especifica trabalhado atualmente.
-        // Percorre minha matriz orginal, indentificando quais vertices apresentam a label atual.
-        for (int i = 0; i < this->g->n; i++) {
-            for (int j = i; j < this->g->n; j++) {
-                // verifica se o indice da matriz = label trabalhada.
-                if(this->g->M[i][j] == aux){
-                    //Preencho meu grafo de trabalho atual.
-                    gAtual.M[i][j] = aux;
-                    gAtual.M[j][i] = aux;
-                    initialPos.push_back(i); // Capturo as posições inicias da minha DFS.
-                    // Preencho lista de adjacencia
-                    gAtual.L[i].push_back(j);
-                    gAtual.L[j].push_back(i);
-                }else{
-                    // Sem cor.
-                    gAtual.M[i][j] = this->g->nLabels;
-                    gAtual.M[j][i] = this->g->nLabels;
-                }
-
-            }
-        }
+    int cor = 0; // Variavell auxiliar (label atual);
+    while(cor != this->g->nLabels){ // Percorre todas as minhas labels, montando as matrizes de cada uma.
         // Grafo atual.
-        printf("Grafo da cor %d:\n", aux);
+        printf("Grafo da cor %d:\n", cor);
         for (int i = 0; i < this->g->n; i++) {
             for (int j = 0; j < this->g->n; j++) {
-                printf("%d ", gAtual.M[i][j]);
+                printf("%d ", this->Ml[cor][i][j]);
 
             }
             printf("\n");
         }
         // DFS nas posições inciais capturadas.
-        for(int k = 0; k<initialPos.size(); k++){
-             printf("Front: %d\n", initialPos[k]);
-             if(gAtual.L[initialPos[k]].size()>1){ // verifica apenas os vertices com mais de uma vizinhança.
-                printf("Fiz DFS\n");
-                gAtual.DFS(initialPos[k], d); // DFS.
-                gAtual.AddVirtualEdges(d, aux);
+        vector<bool> isNewComponent(this->n, true); 
+        for(int v = 0; v<this->n; v++){
+             if(isNewComponent[v])
+             {
+               printf("Front: %d\n", v);
+               if(this->.Ll[cor][v].size() > 1){ // verifica apenas os vertices com mais de uma vizinhança.
+                    this->.DFS(v, d); // DFS.
+                    this->AddVirtualEdges(d, cor);
+                    printf("Fiz DFS\n");
+               }
              }
-             d->virtualVertex.clear();// Limpa meus vertices encontrados na DFS.
         }
-        // Somando meu grafos encontrados em cada cor.
-        for(int i = 0; i< this->g->n; i++){
-            for(int j = i; j< this->g->n; j++){
-                // Restrição para nao somar quando ambos estiverem sem cor.
+                aux++; //prox label.
+        
 
-                if((gVirtual.M[i][j] == 0)||((gVirtual.M[i][j] != this->g->nLabels)&&(gAtual.M[i][j] != this->g->nLabels ))) {
-                    gVirtual.M[i][j] += gAtual.M[i][j];
-                    gVirtual.M[j][i] = gVirtual.M[i][j];
-                }else if(gVirtual.M[i][j]== this->g->nLabels){
-                    gVirtual.M[i][j] = gAtual.M[i][j];
-                    gVirtual.M[j][i] = gVirtual.M[i][j];
-                }
-
-            }
-        }
-      //  aux++; //prox label.
-        // Esvazia minhas posições iniciais ao passar para proxima label.
-        initialPos.clear();
-
-    //}
-    // Grafo original.
-    printf("Grafo ORIGINAL:\n");
-    for (int i = 0; i < this->g->n; i++) {
-            for (int j = 0; j < this->g->n; j++) {
-                printf("%d ", this->g->M[i][j]);
-            }
-            printf("\n");
     }
+   
     printf("Grafo VIRTUAL:\n");
-    for (int i = 0; i < gVirtual.n; i++) {
-            for (int j = 0; j < gVirtual.n; j++) {
-                printf("%d ", gVirtual.M[i][j]);
+    for (int i = 0; i < this->g->n; i++) {
+            for (int j = 0; j < this->this->n; j++) {
+                vector<int> &cores = this->M[i][j];
+                 printf("{");
+                 for(int c=0; c < cores.size(); ++c)
+                    printf("%d ", cores[c]);
+                 printf("} ");
             }
             printf("\n");
     }
